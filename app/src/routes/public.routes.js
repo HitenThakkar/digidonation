810f8b6b96892a1e7cb572a0dc603d1ae7d37507
HitenThakkar <hitenthakkar512@gmail.com>
2023-09-09 15:08:41 +0530

    get all users

--- app/src/routes/public.routes.js
+++ app/src/routes/public.routes.js
@@ -4,4 +4,5 @@ const router = express.Router()
 
 router.post("/signup",sessionController.signup)
 router.post("/login",sessionController.login)
+router.post("/listuser",sessionController.getAllUsers)
 module.exports = router 
