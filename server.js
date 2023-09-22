245cb7f592ea060ac08ad036f98053446d5d175a
HitenThakkar <hitenthakkar512@gmail.com>
2023-09-22 14:40:50 +0530

    admin routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 require("./app/src/config/dbConfig").getDbConnection()
 
 const publicRoutes = require("./app/src/routes/public.routes")
+const adminRoutes = require("./app/src/routes/admin.routes")
 
 const app = express()
 
@@ -14,6 +15,9 @@ app.use(cors())
 
 //public 
 app.use("/public",publicRoutes)
+
+//admin
+app.use(adminRoutes)
 
 app.listen(9099)
 console.log("server started 9099");
